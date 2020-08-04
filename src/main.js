0b2ae6e61308ada77d14448f9876c181709c2bc5
SebastianLob <49219859+SebastianLob@users.noreply.github.com>
2020-08-04 01:52:16 -0300

    Added axios

--- src/main.js
+++ src/main.js
@@ -4,12 +4,16 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+const axios = require('axios');
+axios.defaults.baseURL = 'http://www.sebastian.lob.com.ar/api/';
+axios.defaults.timeout = 10000;
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  axios,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
